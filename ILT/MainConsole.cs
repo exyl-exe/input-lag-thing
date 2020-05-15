b126b8548f633ff79496f0a5a4eb06a14b8e3b43
Exyl <lyxe77@gmx.fr>
2020-05-15 16:24:18 +0200

    Now measuring input lag when releasing. Added the option to change the amount of measures per command.

--- ILT/MainConsole.cs
+++ ILT/MainConsole.cs
@@ -12,6 +12,7 @@ namespace ILT
     static class MainConsole
     {
         public static int framerate = 60;
+        public static int count = 200;
         public static List<Tuple<string, Action>> menuActions = new List<Tuple<string, Action>>();
         public static bool finished = false;
 
@@ -37,7 +38,9 @@ namespace ILT
 
         static void PrintMenu()
         {
-            Console.WriteLine("Action to do :");
+            Console.WriteLine("######################");
+            Console.WriteLine("Measures done per command : " + count);
+            Console.WriteLine("Actions:");
             for (int i = 0; i < menuActions.Count; i++)
             {
                 Console.WriteLine(i + ") " + menuActions[i].Item1);
@@ -49,36 +52,88 @@ namespace ILT
             finished = true;
         }
 
+        static void SetCount()
+        {
+            Console.WriteLine("New value ?");
+            string countString = Console.ReadLine();
+            int countBackup = count;
+            if (int.TryParse(countString, out count) && count>0)
+            {
+                Console.WriteLine("The delay will be measured " + count + " times per command.");
+            } else
+            {
+                Console.WriteLine("Invalid value");
+                count = countBackup;
+            }
+        }
+
         static void InputLagMeasure()
         {
             Random rand = new Random();
             Thread.Sleep(2000);
-            const int count = 500;
             Point p = new Point(1500, 2);
-            long t1, t2;
+            long t1, t2, t3, t4;
+            long diff, diffR;
+            //Values measured after a press
             long average, max, min;
-            bool isRed = false;
-            max = 0;
-            min = 1000;
-            average = 0;
+            //Values measured after a release
+            long averageR, maxR, minR;
+
+            min = minR = 9999999999;//dirty but idc
+            max = maxR = 0;
+            average = averageR = 0;
+
+            //Unpause the game
             InputGen.Send(InputGen.ScanCodeShort.SPACE);
             InputGen.Release(InputGen.ScanCodeShort.SPACE);
+
+            Stopwatch s = Stopwatch.StartNew();
+
             for (int i = 0; i < count; i++)
             {
-                Thread.Sleep(rand.Next(5000, 6000) / 60);
-                Stopwatch s = Stopwatch.StartNew();
+                //Sleeps a random time between 0 and 1 frame, to randomize if the input happens at the beginning or the end of a frame
+                Thread.Sleep(rand.Next(0,1000) / framerate);
+
+                //Press space, then release it
                 t1 = s.ElapsedMilliseconds;
                 InputGen.Send(InputGen.ScanCodeShort.SPACE);
-                while (IsColorRed(ScreenChecker.GetColorAt(p)) == isRed) ;
+                //Wait for screen to go red
+                while (!IsColorRed(ScreenChecker.GetColorAt(p))) ;
                 t2 = s.ElapsedMilliseconds;
-                long diff = t2 - t1;
-                if (diff < min) min = diff;
-                if (diff > max) max = diff;
-                average += diff;
-                Console.WriteLine(diff);
+
+                Thread.Sleep(rand.Next(0,1000) / framerate);
+                t3 = s.ElapsedMilliseconds;
                 InputGen.Release(InputGen.ScanCodeShort.SPACE);
+                //Wait for screen to go blue
+                while (IsColorRed(ScreenChecker.GetColorAt(p))) ;
+                t4 = s.ElapsedMilliseconds;
+
+                //milliseconds needed for screen color to change after inputing
+                diff = t2 - t1;
+                diffR = t4 - t3;
+
+                //Compute average, min, max
+                min = diff < min ? diff : min;
+                max = diff > max ? diff : max;
+                average += diff;
+
+                minR = diffR < minR ? diffR : minR;
+                maxR = diffR > maxR ? diffR : maxR;
+                averageR += diffR;
+
+                Console.WriteLine(diff +"->"+diffR);
+                
             }
-            Console.WriteLine("Max:" + max + ",Min:" + min + ",avg:" + average / count);
+
+            Console.WriteLine("-- Press --");
+            Console.WriteLine("Max : " + max + "ms");
+            Console.WriteLine("Min : " + min + "ms");
+            Console.WriteLine("Average : " + average/count + "ms");
+            Console.WriteLine("");
+            Console.WriteLine("-- Release --");
+            Console.WriteLine("Max : " + maxR + "ms");
+            Console.WriteLine("Min : " + minR + "ms");
+            Console.WriteLine("Average : " + averageR/count + "ms");
         }
 
         static bool IsColorRed(Color c)
@@ -90,6 +145,7 @@ namespace ILT
         {
             menuActions.Add(new Tuple<string, Action>("Exit", Exit));
             menuActions.Add(new Tuple<string, Action>("Input lag measure", InputLagMeasure));
+            menuActions.Add(new Tuple<string, Action>("Set the amount of measures per command", SetCount));
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Initial readme
+Software made in an attempt to measure input lag in GD. Among the results the program computes, the average is the meaningful one.
+
+It's not by any mean 100% accurate, and there might be an offset between the computed values and actual values. It should be somewhat reliable to compare delay between different ways of playing (vsync on/off, rivatuner ...) though.
+
+Since it checks the color of one pixel on the screen, it isn't going to work reliably when GD runs at a higher fps count than a monitor's refresh rate.
