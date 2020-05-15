a86f57170357047af2c5fc9b545a02dc894a2224
Exyl <lyxe77@gmx.fr>
2020-05-15 15:16:21 +0200

    V0

---
+++ ILT/MainConsole.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ILT
+{
+    static class MainConsole
+    {
+        public static int framerate = 60;
+        public static List<Tuple<string, Action>> menuActions = new List<Tuple<string, Action>>();
+        public static bool finished = false;
+
+        static void Main(string[] args)
+        {
+            string choiceString;
+            int choice;
+            BuildMenu();
+            while (!finished)
+            {
+                PrintMenu();
+                choiceString = Console.ReadLine();
+                if (int.TryParse(choiceString, out choice) && choice < menuActions.Count)
+                {
+                    menuActions[choice].Item2();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid action.");
+                }
+            }
+        }
+
+        static void PrintMenu()
+        {
+            Console.WriteLine("Action to do :");
+            for (int i = 0; i < menuActions.Count; i++)
+            {
+                Console.WriteLine(i + ") " + menuActions[i].Item1);
+            }
+        }
+
+        static void Exit()
+        {
+            finished = true;
+        }
+
+        static void InputLagMeasure()
+        {
+            Random rand = new Random();
+            Thread.Sleep(2000);
+            const int count = 500;
+            Point p = new Point(1500, 2);
+            long t1, t2;
+            long average, max, min;
+            bool isRed = false;
+            max = 0;
+            min = 1000;
+            average = 0;
+            InputGen.Send(InputGen.ScanCodeShort.SPACE);
+            InputGen.Release(InputGen.ScanCodeShort.SPACE);
+            for (int i = 0; i < count; i++)
+            {
+                Thread.Sleep(rand.Next(5000, 6000) / 60);
+                Stopwatch s = Stopwatch.StartNew();
+                t1 = s.ElapsedMilliseconds;
+                InputGen.Send(InputGen.ScanCodeShort.SPACE);
+                while (IsColorRed(ScreenChecker.GetColorAt(p)) == isRed) ;
+                t2 = s.ElapsedMilliseconds;
+                long diff = t2 - t1;
+                if (diff < min) min = diff;
+                if (diff > max) max = diff;
+                average += diff;
+                Console.WriteLine(diff);
+                InputGen.Release(InputGen.ScanCodeShort.SPACE);
+            }
+            Console.WriteLine("Max:" + max + ",Min:" + min + ",avg:" + average / count);
+        }
+
+        static bool IsColorRed(Color c)
+        {
+            return c.R > c.B;
+        }
+
+        static void BuildMenu()
+        {
+            menuActions.Add(new Tuple<string, Action>("Exit", Exit));
+            menuActions.Add(new Tuple<string, Action>("Input lag measure", InputLagMeasure));
+        }
+    }
+}
---
+++ ILT/ScreenChecker.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ILT
+{
+    /*
+    Not my code : https://stackoverflow.com/questions/1483928/how-to-read-the-color-of-a-screen-pixel 
+    */
+
+    class ScreenChecker
+    {
+        [DllImport("user32.dll")]
+        static extern bool GetCursorPos(ref Point lpPoint);
+
+        [DllImport("gdi32.dll", CharSet = CharSet.Auto, SetLastError = true, ExactSpelling = true)]
+        public static extern int BitBlt(IntPtr hDC, int x, int y, int nWidth, int nHeight, IntPtr hSrcDC, int xSrc, int ySrc, int dwRop);
+
+        public static Color GetColorAt(Point location)
+        {
+            Bitmap screenPixel = new Bitmap(1, 1, PixelFormat.Format32bppArgb);
+            using (Graphics gdest = Graphics.FromImage(screenPixel))
+            {
+                using (Graphics gsrc = Graphics.FromHwnd(IntPtr.Zero))
+                {
+                    try
+                    {
+                        IntPtr hSrcDC = gsrc.GetHdc();
+                        IntPtr hDC = gdest.GetHdc();
+                        int retval = BitBlt(hDC, 0, 0, 1, 1, hSrcDC, location.X, location.Y, (int)CopyPixelOperation.SourceCopy);
+                        gdest.ReleaseHdc();
+                        gsrc.ReleaseHdc();
+                    }
+                    catch (System.ArgumentException)
+                    {
+                        Console.WriteLine("Error occured");
+                    }
+                }
+            }
+            return screenPixel.GetPixel(0, 0);
+        }
+    }
+}
